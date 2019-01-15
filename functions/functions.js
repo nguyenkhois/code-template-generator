3909e7e625e6949da90eaed45cc5275fb96dff92
Khoi Le <minarek@outlook.com>
2019-01-15 11:03:01 +0100

    Done for the patch version 2.0.2

--- functions/functions.js
+++ functions/functions.js
@@ -117,12 +117,12 @@ function filterByProperty(objectArray, sPropertyName, sSeekingValue) {
 // Print out message
 function printOutResolve(result) {
     const regularExpression = /component/gi;
+    const seekingComponentText = regularExpression.test(result.content);
 
-    if (regularExpression.test(result.content)){
-        console.log(`\n\x1b[32mDone!\x1b[0m ${result.content} is generated successfully.\n`);
-    } else {
-        console.log(`\n\x1b[32mDone!\x1b[0m ${result.content} ${result.type} is generated successfully.\n`);
-    }
+    console.log(`\n\x1b[32mDone!\x1b[0m ` +
+        `${result.content} ` +
+        `${!seekingComponentText ? result.type + ' ' : ''}` +
+        `is generated successfully.\n`);
 }
 
 function printOutReject(error) {
